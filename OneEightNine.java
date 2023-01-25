b43c3d0d1ee8f9af31d44d04bf17729c86c99053
Mona Lisa <holmesmainsend@gmail.com>
2023-01-25 11:29:22 -0500

    first solution to problem found with given array nums

--- OneEightNine.java
+++ OneEightNine.java
@@ -1,35 +1,37 @@
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class OneEightNine {
     public static void main(String[] args) {
         // Problem specifies that array must have at least length of 1, otherwise would add check here
-        // Problem specifies number of steps must be >= 0, otherwise would factor left shifts in
+        // Problem specifies number of steps k must be >= 0, otherwise would factor left shifts in
 
         int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        // if(nums.length == 1) {
-        //     return statement here
-        // }
-
-        // In problem itself var steps is called k
-        int steps = 6;
-        int netSteps = steps % nums.length;
-        int currentIndex = nums.length - 1 - netSteps;
-        int startIndex = 0;
-
-        LinkedList<Integer> answer = new LinkedList<>();
-
-        while(currentIndex != startIndex) {
-            currentIndex++;
-            answer.addLast(nums[currentIndex]);
-            if(answer.size() == 1) {
-                startIndex = currentIndex - 1;
-            }
-
-            if(currentIndex == nums.length - 1) {
-                currentIndex = -1;
-            }
+        if(nums.length == 1) {
+            // return nums statement here
+            System.out.println(Arrays.toString(nums));
         }
 
-        System.out.println(answer);
+        int steps = 15;
+        int netStepsRight = steps % nums.length;
+        
+        if(netStepsRight == 0) {
+            // return nums statement here
+            System.out.println(Arrays.toString(nums));
+        } else {
+            LinkedList<Integer> answer = new LinkedList<>();
+            int currentIndex = nums.length - netStepsRight;
+
+            while(answer.size() < nums.length) {
+                answer.addLast(nums[currentIndex]);
+                currentIndex++;
+                if(currentIndex == nums.length) {
+                    currentIndex = 0;
+                }
+            }
+    
+            // return answer statement here
+            System.out.println(answer);
+        }
     }
 }
