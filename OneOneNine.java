e8625ac9e22d9ddbc4de22890312bee7e842fa4d
Mona Lisa <holmesmainsend@gmail.com>
2023-01-29 14:30:21 -0500

    already seeing the issues of trying to solve this problem with a traditional 2D array

--- OneOneNine.java
+++ OneOneNine.java
@@ -6,27 +6,6 @@ public class OneOneNine {
     public static void main(String[] args) {
         int rowIndex = 3;
 
-        List<List<Integer>> listOfLists = new ArrayList<>();
-
-        // Adding rows 0 and 1 manually, to focus the loops on all additional rows
-        List<Integer> row0 = new ArrayList<>();
-        row0.add(1);
-        listOfLists.add(row0);
-        List<Integer> row1 = new ArrayList<>();
-        row1.add(1);
-        row1.add(1);
-        listOfLists.add(row1);
-
-        for(int i = 2; i <= rowIndex; i++) {
-            List<Integer> row = new ArrayList<>();
-            row.add(1);
-            for(int cellIndex = 1; cellIndex < i; cellIndex++) {
-                Integer cellValue = listOfLists.get(0);
-                row.add(cellValue);
-            }
-            row.add(1);
-        }
-
-        System.out.println(listOfLists.get(rowIndex));
+        int[][] arrayOfArrays = new int[0][0];
     }
 }
