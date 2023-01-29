f0e0d3b71b8f494499a9b9075f5d2993477950aa
Mona Lisa <holmesmainsend@gmail.com>
2023-01-29 14:12:11 -0500

    starting new approach of isolating rows zero and one, since those rows are unique

--- OneOneNine.java
+++ OneOneNine.java
@@ -4,10 +4,19 @@ import java.util.ArrayList;
 public class OneOneNine {
     // Most return statements would occur where println statements are here
     public static void main(String[] args) {
-        int rowIndex = 3;
+        int rowIndex = 0;
 
         List<List<Integer>> listOfLists = new ArrayList<>();
 
-        // System.out.println(listOfLists.get(3));
+        // Adding rows 0 and 1 manually, to focus the loops on all additional rows
+        List<Integer> row0 = new ArrayList<>();
+        row0.add(1);
+        listOfLists.add(row0);
+        List<Integer> row1 = new ArrayList<>();
+        row1.add(1);
+        row1.add(1);
+        listOfLists.add(row1);
+
+        System.out.println(listOfLists.get(rowIndex));
     }
 }
