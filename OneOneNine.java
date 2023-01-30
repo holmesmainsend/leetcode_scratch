fda8bc5091c8a9b6cfa12ce251436536a66bf9f9
Mona Lisa <holmesmainsend@gmail.com>
2023-01-30 14:06:36 -0500

    analyzing correct solution posted by different user

--- OneOneNine.java
+++ OneOneNine.java
@@ -1,11 +1,17 @@
 import java.util.List;
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class OneOneNine {
-    // Most return statements would occur where println statements are here
+    // Accepted solution from different LeetCode user
     public static void main(String[] args) {
         int rowIndex = 3;
 
-        int[][] arrayOfArrays = new int[0][0];
+        List<Integer> ret = new LinkedList<Integer>();
+        for (int row = 0; row <= rowIndex; row++) {
+            ret.add(0, 1);
+            for (int i = 1; i < row; i++)
+                ret.set(i, ret.get(i) + ret.get(i + 1));
+        }
+        System.out.println(ret);
     }
 }
