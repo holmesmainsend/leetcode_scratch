3026aa1ebdf02881ee597461da47b7cc726a43b3
Mona Lisa <holmesmainsend@gmail.com>
2023-02-01 14:01:35 -0500

    first successful answer to problem

---
+++ OneFiveOne.java
@@ -0,0 +1,27 @@
+import java.util.List;
+import java.util.LinkedList;
+
+public class OneFiveOne {
+    public static void main(String[] args) {
+        String s = "the sky is blue";
+        List<String> intermediate = new LinkedList<String>();
+
+        String word = "";
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) != ' ')
+                word += s.charAt(i);
+            else {
+                intermediate.add(word);
+                intermediate.add(" ");
+                word = "";
+            }
+        }
+        intermediate.add(word);
+
+        String output = "";
+        for(int i = intermediate.size() - 1; i >= 0; i--)
+            output += intermediate.get(i);
+
+        System.out.println(output);
+    }   
+}
