1cc0c89de6c44dc2291adce3c8e7c49d2f66f62f
Mona Lisa <holmesmainsend@gmail.com>
2023-02-08 12:44:58 -0500

    arrived at accepted answer

--- FiveFiveSeven.java
+++ FiveFiveSeven.java
@@ -5,12 +5,22 @@ public class FiveFiveSeven {
         String s = "Let's take LeetCode contest";
         char[] a = s.toCharArray();
         System.out.println(Arrays.toString(a));
-        int i = 0, j = a.length - 1;
-        while(i < j) {
-            char t = a[i];
-            a[i++] = a[j];
-            a[j--] = t;
+        int i = 0, j = 0, n = a.length;
+
+        while(i < n) {
+            while(i < j || i < n && a[i] == ' ') i++;
+            while(j < i || j < n && a[j] != ' ') j++;
+            reverse(a, i, j - 1);
         }
-        System.out.println(new String(a).substring(0, i));
+
+        System.out.println(new String(a).substring(0, n));
+    }
+
+    public static void reverse(char[] a, int i, int j) {
+        while(i < j) {
+            char temp = a[i];
+            a[i++] = a[j];
+            a[j--] = temp;
+        }
     }
 }
