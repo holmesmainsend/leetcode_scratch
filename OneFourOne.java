f02d1b1a9490dbd3a30d7a43b39e7a4687a5e24a
Mona Lisa <holmesmainsend@gmail.com>
2023-02-12 15:52:55 -0500

    exmining top solution from LeetCode user

---
+++ OneFourOne.java
@@ -0,0 +1,18 @@
+public class OneFourOne {
+    public static void main(String[] args) {
+        // Examining top solution from another user
+        // public boolean hasCycle(ListNode head) {
+        //     ListNode slow = head, fast = head;
+            
+        //     while (fast != null && fast.next != null) {
+        //       slow = slow.next;
+        //       fast = fast.next.next;
+              
+        //       if (slow == fast) 
+        //           return true;
+        //     }
+            
+        //     return false;
+        //   }
+    }
+}
